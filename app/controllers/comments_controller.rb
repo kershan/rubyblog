6cf2e57b7f1d4e26dbd5348113d576690077fded
Kershan Naidoo <n.kershan@gmail.com>
2014-07-02 22:13:37 +0200

    Security fix in comment controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,7 @@ class CommentsController < ApplicationController
 	
 	http_basic_authenticate_with name: "dhh",
 	password: "secret",
-	only :destroy
+	only: :destroy
 
 	def create
 		@article = Article.find(params[:article_id])
