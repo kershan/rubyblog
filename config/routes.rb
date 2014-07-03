0b4d40dee0253b4f85fc55e1989d5ace15ec5a8b
Kershan Naidoo <n.kershan@gmail.com>
2014-07-03 14:08:56 +0200

    Devise installed and user model created

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'welcome/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
@@ -6,6 +7,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   Blog::Application.routes.draw do
+  devise_for :users
     resources :articles do
       resources :comments
     end
