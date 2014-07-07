18cdacd774d5dd0dc7656e04188f3b4a8007c163
Kershan Naidoo <n.kershan@gmail.com>
2014-07-07 11:46:00 +0200

    Two simple tests added for article controllers

--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -1,8 +1,17 @@
 require 'test_helper'
 
 class ArticlesControllerTest < ActionController::TestCase
-    test "should get index" do
-    get :index
-    assert_response :success
-  end
+
+	include Devise::TestHelpers
+
+	test "login test" do
+		sign_in users(:one)
+		assert_response :success, "Login test"
+	end
+
+	test "should get index" do
+		get :index
+		assert_response :success, "gets index"
+	end
+
 end
